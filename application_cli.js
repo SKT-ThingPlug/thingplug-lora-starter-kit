fb1c09e6faa38b9adfeadb33fcc2052ed9bf2051
skt.thingplug <skt.thingplug@gmail.com>
2016-10-24 09:12:12 +0900

    delete nodeRI in application_cli.js

--- application_cli.js
+++ application_cli.js
@@ -27,7 +27,7 @@ var cmd = process.argv[3];
 var httpReq = require('./promise-http').request;
 
 if(cmt != null && cmd != null){
-	reqMgmtCmd(cmt, "{\"cmd\":\""+cmd+"\"}", config.nodeRI, function(err, data){
+	reqMgmtCmd(cmt, "{\"cmd\":\""+cmd+"\"}", function(err, data){
 	  console.log("{\"cmd\":\""+cmd+"\"}");
 	  console.log("{\"cmt\":\""+cmt+"\"}");
 	  console.log(colors.green("======================"));
@@ -80,7 +80,7 @@ httpReq({
 
 //---------------------------------------------------- 2. mgmCmd 요청----------------------------------------------------------//
 
-function reqMgmtCmd(mgmtCmdPrefix, cmd, nodeRI, cb){
+function reqMgmtCmd(mgmtCmdPrefix, cmd, cb){
 // 2. mgmCmd 요청
 httpReq({ 
     options: {
@@ -99,8 +99,7 @@ httpReq({
 		body : {mgc:{
     exra : cmd,			//제어 요청(일반적으로 원격 장치를 RPC호출)을 위한 Argument 정의 (exra == execReqArgs)
     exe : true,						//제어 요청 Trigger 속성으로 해당 속성은 (True/False로 표현) (exe == execEnabler)
-	cmt : mgmtCmdPrefix,
-	ext : nodeRI
+	cmt : mgmtCmdPrefix
   }}
 }).then(function(result){
   console.log(colors.green('mgmtCmd 제어 요청'));

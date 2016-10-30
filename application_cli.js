5e58174152ddd123590d602fa95177f5c32dc58c
skt.thingplug <skt.thingplug@gmail.com>
2016-10-30 11:20:18 +0900

    application comments

--- application_cli.js
+++ application_cli.js
@@ -1,20 +1,3 @@
-/*
- ThingPlug StarterKit for LoRa version 0.1
- 
- Copyright © 2016 IoT Tech. Lab of SK Telecom All rights reserved.
-
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
-	http://www.apache.org/licenses/LICENSE-2.0
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
-
-*/
-
 'use strict';
 var colors = require('colors');
 var async = require('async');
@@ -26,6 +9,8 @@ var cmd = process.argv[3];
 
 var httpReq = require('./promise-http').request;
 
+
+//-----------------------------------------------Get cmt & cmd Argument from CLI----------------------------------------------//
 if(cmt != null && cmd != null){
 	reqMgmtCmd(cmt, "{\"cmd\":\""+cmd+"\"}", function(err, data){
 	  console.log("{\"cmd\":\""+cmd+"\"}");
@@ -33,26 +18,26 @@ if(cmt != null && cmd != null){
 	  console.log(colors.green("======================"));
 	  });
 }
+//=============================================================================================================================//
+//-----------------------------------------------Get latest Contents----------------------------------------------//
 setInterval( function(){
 	getLatestContainer(function(err, data){
 		console.log('content : ' + data.cin.con);
 		console.log('resouceId : ' + data.cin.ri);
-		console.log('생성일 : '+ data.cin.ct);
+		console.log('Creation Time : '+ data.cin.ct);
 	});
   },1000);
 
 //=============================================================================================================================//
 
 
-
-//--------------------------------------------Request ID를 생성하기 위한 RandomInt Function------------------------------------//
+//-----------------------------------------------randomInt Function for Create Request ID--------------------------------------//
 function randomInt (low, high) {
 	return Math.floor(Math.random() * (high - low + 1) + low);
 }
 //=============================================================================================================================//
 
-
-//-------------------------------- 1. Container에 저장된 최신 값 조회(Retrieve)----------------------------------------//
+//-------------------------------- 1. GET latest Contents (Retrieve)----------------------------------------//
 function getLatestContainer(cb){
 httpReq({ 
   options: {
@@ -61,16 +46,17 @@ httpReq({
     path : '/'+config.AppEUI+'/'+config.version+'/remoteCSE-'+ config.nodeID+ '/container-'+config.containerName+'/latest',
     method: 'GET',
     headers : {
-      Accept: 'application/json',
-      locale: 'ko',
-      uKey : config.uKey,
-      'X-M2M-RI': config.nodeID+'_'+randomInt(100000, 999999),
-      'X-M2M-Origin': config.nodeID
+      Accept: 'application/json',												// Originator may use the Accept header to indicate which content-type is supported. 
+      uKey : config.uKey,											// user Token Key from ThingPlug Portal
+      'X-M2M-RI': config.nodeID+'_'+randomInt(100000, 999999),		// X-M2M-RI header shall be mapped to the Request Identifier parameter
+      'X-M2M-Origin': config.nodeID								// X-M2M-Origin header value shall be specified by the composer of the request(originator)
     }
   }
 }).then(function(result){
   if(result.data){
 		var data = JSON.parse(result.data);
+		
+
 		return cb(null, data);
   }
 });
@@ -78,31 +64,29 @@ httpReq({
 
 //=============================================================================================================================//
 
-//---------------------------------------------------- 2. mgmCmd 요청----------------------------------------------------------//
-
+//---------------------------------------------------- 2. Request mgmtCmd(PUSH MESSAGE)----------------------------------------------------------//
 function reqMgmtCmd(mgmtCmdPrefix, cmd, cb){
-// 2. mgmCmd 요청
-httpReq({ 
+	httpReq({ 
     options: {
       host : config.TPhost,
       port : config.TPport,
       path : '/'+config.AppEUI+'/'+config.version+'/mgmtCmd-'+config.nodeID + '_' + mgmtCmdPrefix,
       method: 'PUT',
       headers : {
-        Accept: 'application/json',
-        uKey : config.uKey,
-        'X-M2M-Origin': config.nodeID,
-        'X-M2M-RI': config.nodeID+'_'+randomInt(100000, 999999),
+        Accept: 'application/json',									// Originator may use the Accept header to indicate which content-type is supported.
+        uKey : config.uKey,											// user Token Key from ThingPlug Portal
+        'X-M2M-Origin': config.nodeID,								// X-M2M-Origin header value shall be specified by the composer of the request(originator)
+        'X-M2M-RI': config.nodeID+'_'+randomInt(100000, 999999),	// X-M2M-RI header shall be mapped to the Request Identifier parameter
 		'Content-Type': 'application/json;ty=8'
 	  }
       },
 		body : {mgc:{
-    exra : cmd,			//제어 요청(일반적으로 원격 장치를 RPC호출)을 위한 Argument 정의 (exra == execReqArgs)
-    exe : true,						//제어 요청 Trigger 속성으로 해당 속성은 (True/False로 표현) (exe == execEnabler)
-	cmt : mgmtCmdPrefix
+    exra : cmd,				// (exra == execReqArgs)
+    exe : true,				// Trigger Attribute (true/false) (exe == execEnable)
+	cmt : mgmtCmdPrefix		// command Type
   }}
 }).then(function(result){
-  console.log(colors.green('mgmtCmd 제어 요청'));
+  console.log(colors.green('Request mgmtCmd'));
   if(result.data){
 		var data = JSON.parse(result.data);
 		return cb(null, data);
